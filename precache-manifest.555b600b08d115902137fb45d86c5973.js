4686323fc246fd76dedad2b92830c1dd0f791ae2
Lautaro <lautaroamarchetti@gmail.com>
2022-06-09 15:30:01 -0300

    Updates

---
+++ precache-manifest.555b600b08d115902137fb45d86c5973.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9706f046d721ad35030",
+    "url": "/testimonios-freeCodeCamp/static/css/main.ae3aea56.chunk.css"
+  },
+  {
+    "revision": "e9706f046d721ad35030",
+    "url": "/testimonios-freeCodeCamp/static/js/main.e9706f04.chunk.js"
+  },
+  {
+    "revision": "ccfaed377a9312a06ca7",
+    "url": "/testimonios-freeCodeCamp/static/js/1.ccfaed37.chunk.js"
+  },
+  {
+    "revision": "30dc820f0a6f7b38524e",
+    "url": "/testimonios-freeCodeCamp/static/js/runtime~main.30dc820f.js"
+  },
+  {
+    "revision": "ec6318e89bd5de45e531f3dac9281fce",
+    "url": "/testimonios-freeCodeCamp/static/media/testimonio-emma.ec6318e8.png"
+  },
+  {
+    "revision": "30aa65c477fbbb29dd8783fd97fe0f84",
+    "url": "/testimonios-freeCodeCamp/static/media/testimonio-sarah.30aa65c4.png"
+  },
+  {
+    "revision": "788e28f720a4e1e00d3292f95fddc594",
+    "url": "/testimonios-freeCodeCamp/static/media/testimonio-shawn.788e28f7.png"
+  },
+  {
+    "revision": "2771252c5bb8dbeff6ceb30e398024d7",
+    "url": "/testimonios-freeCodeCamp/index.html"
+  }
+];
